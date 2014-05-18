8ed7e543a9fac698fa6b36c2617d586c51ac7de6
ILJICH <iljich@iljich.name>
2014-05-18 23:41:13 +0400

    WIP. Need to rework physics.

--- prototype.py
+++ prototype.py
@@ -3,7 +3,7 @@
 This file contains code that is to be revised and split in parts later.
 Purely a playground.
 """
-from math import sin, pi, cos, copysign
+from math import sin, pi, cos, copysign, sqrt
 from random import randint
 import pyglet
 from pyglet.gl import GL_TRIANGLES, GL_POINTS
@@ -16,14 +16,15 @@ class Creep(object):
     world = None
 
     forward_acc = 3
-    rotatory_acc = 0.02
+    rotatory_acc = 0.0005
 
     min_speed = 1
 
-    def __init__(self, location, rotation, size):
+    def __init__(self, location, rotation, size, color):
         self.location = location
         self.rotation = rotation
         self.size = size
+        self.color = color
         self.speed = [0, 0]
         self.speed_rot = 0
         self.target = self.location
@@ -65,6 +66,11 @@ class Creep(object):
 
 class Drawer(object):
 
+    def tick(self, creeps):
+        for creep in creeps:
+            self.draw(creep)
+            self._draw_target(creep)
+
     def draw(self, creep):
         # creep is a triangle
         x, y = creep.location
@@ -76,15 +82,14 @@ class Drawer(object):
              (x + s * cos(r + 0), y + s * sin(r + 0),
               x + s * cos(r + 4 * pi / 3), y + s * sin(r + 4 * pi / 3),
               x + s * cos(r + 8 * pi / 3), y + s * sin(r + 8 * pi / 3))),
-            ('c3B', (150, 0, 0) * 3)
+            ('c3B', creep.color * 3)
         )
 
     def _draw_target(self, creep):
-        x, y = creep.target
         pyglet.graphics.draw(
             1, GL_POINTS,
-            ('v2f', (x, y)),
-            ('c3B', (150, 0, 0))
+            ('v2f', creep.target),
+            ('c3B', creep.color)
         )
 
 
@@ -93,7 +98,11 @@ class World(object):
     creeps = []
 
     damping_fwd = 0.1
-    damping_rot = 30
+    damping_rot = 0.000001
+
+    def spawn_creep(self, *args, **kwargs):
+        creep = Creep(*args, **kwargs)
+        self.register_creep(creep)
 
     def register_creep(self, creep):
         self.creeps.append(creep)
@@ -101,22 +110,16 @@ class World(object):
 
     def creep_move(self, creep, forward_acc, rotatory_acc):
         r = creep.rotation
-        creep.speed[0] += (
-            forward_acc * cos(r) -
-            copysign(self.damping_fwd * (creep.speed[0] ** 2), creep.speed[0])
-        )
-        creep.speed[1] += (
-            forward_acc * sin(r) -
-            copysign(self.damping_fwd * (creep.speed[1] ** 2), creep.speed[1])
-        )
-        # TODO: rotatory speed should be influenced by forward speed
-        creep.speed_rot += (
-            rotatory_acc -
-            copysign(
-                self.damping_rot * (creep.speed_rot ** 2),
-                creep.speed_rot
-            )
-        )
+        # applied force
+        fax, fay = forward_acc * cos(r), forward_acc * sin(r)
+        frx = copysign(self.damping_fwd * (creep.speed[0] ** 2), creep.speed[0])
+        fry = copysign(self.damping_fwd * (creep.speed[1] ** 2), creep.speed[1])
+        # TODO: that needs a little bit closer model
+        creep.speed[0] += (fax - frx)
+        creep.speed[1] += (fay - fry)
+
+        rot_res = self.damping_rot * sqrt(creep.speed[0] ** 2 + creep.speed[1] ** 2)
+        creep.speed_rot += (rotatory_acc - rot_res)
 
     def tick(self):
         for creep in self.creeps:
@@ -127,18 +130,19 @@ class World(object):
 
 window = pyglet.window.Window(1024, 768)
 
-drawer = Drawer()
 world = World()
-creep = Creep([0, 0], 0, 40)
-world.register_creep(creep)
+drawer = Drawer()
+world.spawn_creep([10, 10], 0.1, 40, (250, 25, 25))
+world.spawn_creep([1000, 10], 2, 40, (25, 250, 25))
+world.spawn_creep([10, 700], 6, 40, (25, 25, 250))
 
 
 def update(dt):
     window.clear()
-    creep.move()
+    for creep in world.creeps:
+        creep.move()
     world.tick()
-    drawer.draw(creep)
-    drawer._draw_target(creep)
+    drawer.tick(world.creeps)
 
 pyglet.clock.schedule_interval(update, 1/60.0)
 
